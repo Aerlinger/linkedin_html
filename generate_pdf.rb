f7f899c9baa5995ea548a9604bc81a1af81bdfbc
Anthony Erlinger <aerlinger@gmail.com>
2014-08-03 23:40:07 -0400

    Add some hacks/tweaks to get the HTML to render properly in PDF

---
+++ generate_pdf.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'doc_raptor'
+
+dpi = (ARGV && ARGV.first) || 100
+
+DocRaptor.api_key "HbEeYNkfFXaqNUhLSzRa"
+
+pdf_html = File.read("anthony_erlinger_resume.html")
+test = true
+
+File.open("anthony_erlinger_resume#{test ? "_test" : ""}.pdf", "w+b") do |f|
+  f.write DocRaptor.create :document_content => pdf_html,
+                           :name             => "anthony_erlinger_resume#{test ? "_test" : ""}.pdf",
+                           :document_type    => "pdf",
+                           :test             => test,
+                           :prince_options    => {
+                             css_dpi: dpi
+                           }
+end
+
